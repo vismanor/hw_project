2263aeb399cee34e555b4ff382382b2ffc96bd40
Katya Smirnova <vismanork@gmail.com>
2023-11-30 04:49:52 +0300

    bag is fixed

--- str_func.py
+++ str_func.py
@@ -1,8 +1,12 @@
 def str_func(value: str):
+
     """Функция возвращает буквы капсом"""
+
     return value.upper()
 
 
 def capitalize_func(value: str):
+
     """Функция, которая делает заглавными первые буквы каждого слова"""
+
     return ' '.join(word.capitalize() for word in value.split())

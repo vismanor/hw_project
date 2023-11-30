ffc790c034df02dfd82d85565b6ee445cd307acf
Katya Smirnova <vismanork@gmail.com>
2023-11-30 03:53:25 +0300

    Конфликт решен + Добавлена функция, которая "заглавит" первую букву слова + докстринг кричащей функции

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(value: str):
+    """Функция возвращает буквы капсом"""
     return value.upper()
+
+
+def capitalize_func(value: str):
+    """Функция, которая делает заглавными первые буквы каждого слова"""
+    return ' '.join(word.capitalize() for word in value.split())
